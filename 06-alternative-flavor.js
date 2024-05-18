46355d41834c1284565761e9f732cf8c484713f6
Caua Ribas <cauaribasone@gmail.com>
2024-05-18 17:57:15 -0300

    first commit

---
+++ 06-alternative-flavor.js
@@ -0,0 +1,6 @@
+module.exports.items = ['item1', 'item2', 'item3', 'item4', 'item5'];
+const person = {
+    name: 'bob',
+}
+
+module.exports.singlePerson = person;
---
+++ app.js
@@ -0,0 +1,18 @@
+// npm - global command, comes with node
+// npm --version or npm -v
+
+// local dependency - use it only in this particular project
+// npm i <packageName>
+
+// global dependency - use it in any project
+// npm install -g <packageName>
+// sudo npm install -g <packageName> (mac)
+
+const _ = require('lodash');
+
+const items = [1, [2, [3, [4]]]];
+
+const newItems = _.flattenDeep(items);
+
+console.log(items);
+console.log(newItems);
