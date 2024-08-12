a5da41463d4bfc07263101d655d93d15099a4b64
Caua Ribas <cauaribasone@gmail.com>
2024-08-12 16:35:21 -0300

    test

--- 04-names.js
+++ 04-names.js
@@ -3,6 +3,7 @@ const secret = "SUPER SECRET";
 // share
 const john = "john";
 const peter = "peter";
+const carl = "carl";
 
 console.log(module);
-module.exports = { john, peter };
+module.exports = { john, peter, carl };
